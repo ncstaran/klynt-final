67a06a3db22b30daf9a5783b3ec41348550591b1
Nastaran <ncstaran@gmail.com>
2023-06-08 23:07:51 +0200

    bye

--- miniPlayerData.js
+++ miniPlayerData.js
@@ -1,15 +1,15 @@
 klynt.miniPlayerData = {
-    "thanksForWatchingWording": "Thanks for watching!",
     "analyticsKey": "",
-    "fullscreenInfoWording": "This program will launch in fullscreen",
-    "yesWording": "Yes",
     "title": "site",
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "thanksForWatchingWording": "Thanks for watching!",
+    "resumePlaybackWording": "Resume playback?",
+    "downloadAppWording": "Download App",
+    "yesWording": "Yes",
+    "redirectToMobileApp": "auto",
+    "launchAppWording": "Then Launch Project",
+    "url": "",
     "thumbnail": "Medias/Photos/school.jpg",
     "noWording": "No",
-    "resumePlaybackWording": "Resume playback?",
-    "redirectToMobileApp": "auto",
-    "downloadAppWording": "Download App",
-    "url": "",
-    "launchAppWording": "Then Launch Project",
     "description": ""
 }
