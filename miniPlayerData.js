7ba56790e134981940f42240e213af9e62b5fbd2
Nastaran <ncstaran@gmail.com>
2023-06-08 20:27:08 +0200

    moved the files

---
+++ miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "resumePlaybackWording": "Resume playback?",
+    "yesWording": "Yes",
+    "redirectToMobileApp": "auto",
+    "thanksForWatchingWording": "Thanks for watching!",
+    "noWording": "No",
+    "analyticsKey": "",
+    "title": "site",
+    "launchAppWording": "Then Launch Project",
+    "url": "",
+    "downloadAppWording": "Download App",
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "thumbnail": "Medias/Photos/school.jpg",
+    "description": ""
+}
